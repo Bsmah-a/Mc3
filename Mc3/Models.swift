e69e5cf6c4f72bbaade8d3bb6b0d4b558e760072
RenadAlqarni <renadsalqarni@gmail.com>
2024-01-30 15:28:14 +0300

    Ozzy

---
+++ Mc3/Models.swift
@@ -0,0 +1,18 @@
+//
+//  Models.swift
+//  Mc3
+//
+//  Created by Renad Alqarni on 29/01/2024.
+//
+
+import SwiftUI
+
+struct Models: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Models()
+}
